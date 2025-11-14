187e85fcfb7b3390753c8da4c682904f99a9ad42
ELSEFI <mohamedelsafy.as@gmail.com>
2025-11-14 14:29:49 +0200

    add Update Product Route

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -23,4 +23,11 @@ const protect = async (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    return next();
+  }
+  res.status(403).json({ message: "Not authorized as an admin" });
+};
+
+module.exports = { protect, admin };
--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const Product = require("../models/Product");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // ROUTE POST /api/products CREATE NEW PRODUCT (PRIVATE => ADMIN)
 
-router.post("/", protect, async (req, res) => {
+router.post("/", protect, admin, async (req, res) => {
   try {
     const {
       name,
@@ -55,6 +55,68 @@ router.post("/", protect, async (req, res) => {
 
     const createdProduct = await product.save();
     res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// ROUTE PUT /api/products/:id UPDATE PRODUCT (PRIVATE => ADMIN)
+
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      const updateProduct = await product.save();
+      res.json(updateProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
