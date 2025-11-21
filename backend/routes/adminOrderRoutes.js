3a41f0e169af92068a5f06a47ad9b2d561d429f5
ELSEFI <mohamedelsafy.as@gmail.com>
2025-11-21 18:16:09 +0200

    Add Update Orders (Admin) Route

--- backend/routes/adminOrderRoutes.js
+++ backend/routes/adminOrderRoutes.js
@@ -14,4 +14,26 @@ router.get("/", protect, admin, async (req, res) => {
   }
 });
 
+// ROUTE PUT /api/admin/orders/:id UPDATE ORDER STATUS (ADMIN ONLY) (PRIVATE/ADMIN)
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) return res.status(400).json({ message: "Order Not Found" });
+
+    order.status = req.body.status || order.status;
+    order.isDelivered =
+      req.body.status === "Delivered" ? true : order.isDelivered;
+    order.deliveredAt =
+      req.body.status === "Delivered" ? Date.now() : order.deliveredAt;
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+
+
 module.exports = router;
